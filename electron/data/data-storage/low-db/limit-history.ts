214da597759c1a3f870e154fa9d70da6d0e15fbb
dongzhigang <dongzhigang@megvii.com>
2023-10-20 19:59:56 +0800

    feat: 修复题材同步报错问题

--- electron/data/data-storage/low-db/limit-history.ts
+++ electron/data/data-storage/low-db/limit-history.ts
@@ -55,15 +55,21 @@ export const updateTopicForLimitStocksByDay = async (date: string, items: Array<
   if (table.data.name !== 'limit-history') return new Error('数据库表不匹配');
   const orgStocks = table.data.rows.find(r => r.date === date);
   if (!orgStocks) return new Error('未找指定日期的数据');
+  if(orgStocks.items.length !== items.length) return new Error('涨停数组与题材数组长度不等,请检查数据');
   orgStocks.items.forEach(s => {
-    const target = items.find(v => v.name === s.name);
-    if (type === 'tdx') {
-      if (!Array.isArray(s.tdx_topics)) s.tdx_topics = [];
-      if (s.tdx_topics.length === 0) s.tdx_topics.push(...target.tdx_topics);
-    } else {
-      if (!Array.isArray(s.ths_topics)) s.ths_topics = [];
-      if (s.ths_topics.length === 0) s.ths_topics.push(...target.ths_topics);
+    const target = items.find(v => v.code === s.code);
+    if(target !== undefined){
+      if (type === 'tdx') {
+        if (!Array.isArray(s.tdx_topics)) s.tdx_topics = [];
+        if (s.tdx_topics.length === 0) s.tdx_topics.push(...target.tdx_topics);
+      } else {
+        if (!Array.isArray(s.ths_topics)) s.ths_topics = [];
+        if (s.ths_topics.length === 0) s.ths_topics.push(...target.ths_topics);
+      }
+    }else{
+      console.warn(`未找到对应股票${s.name}`);
     }
+
   });
   try {
     await table.write();
